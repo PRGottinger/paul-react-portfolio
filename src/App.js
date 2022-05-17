a0be30eab19d811e31785213ea1c7d382cbb518f
Paul Gottinger <pgottinger@outlook.com>
2022-05-17 00:00:02 -0500

    portfolio basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  // const page = "Contact";
+  const [page, setPage] = useState("About");
+
+  const render = () => {
+    if (page == "About") {
+      return <About></About>;
+    } else if (page == "Contact") {
+      return <Contact></Contact>;
+    } else if (page == "Projects") {
+      return <Projects></Projects>;
+    } else {
+      return <Resume></Resume>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button
+        onClick={() => {
+          setPage("Contact");
+        }}
+      >
+        click here
+      </button> */}
+      <Nav setPage={setPage}></Nav>
+      <main>{render()}</main>
     </div>
   );
 }
 
 export default App;
+
+// change
---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Resume(props) {
+  return (
+    <div>
+      <h1>Paul's Resume</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Resume;
