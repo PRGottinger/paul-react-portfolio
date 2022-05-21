cb655bc5445d420490bb6755247de83c046fa2ef
Paul Gottinger <pgottinger@outlook.com>
2022-05-21 01:03:33 -0500

    resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,9 +4,19 @@ function Resume(props) {
   return (
     <div>
       <h1>Paul's Resume</h1>
+      <br/>
+      <div>
+      <h4>University of Minnesota's Full-Stack Web Development Bootcamp</h4>
       <ul>
-        <li></li>
+        <li>HTML, CSS, Javscript, JQuery</li>
+        <li>Web APIs, Third-Party APIs, Server-Side APIs</li>
+        <li>Node.js, Object-Oriented Programming, Express.js</li>
+        <li>Object-Relational Mapping, Model-View-Controller</li>
+        <li>SQL, NoSQL (MongoDB), GraphQL</li>
+        <li>Progressive Web Applications, Regex statments, React</li>
+
       </ul>
+      </div>
     </div>
   );
 }
